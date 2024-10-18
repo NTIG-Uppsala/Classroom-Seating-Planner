91ccbf6a3782cb3503b8425012d78465cce4c197
JCTe4 <jesper.cejie@elev.ga.ntig.se>
2024-10-18 16:00:22 +0200

    started implementing the classlist into the UI

--- Classroom-Seating-Planner/Classroom-Seating-Planner/MainWindow.xaml.cs
+++ Classroom-Seating-Planner/Classroom-Seating-Planner/MainWindow.xaml.cs
@@ -16,9 +16,56 @@ namespace Classroom_Seating_Planner
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Define the global list of names here
+        private List<string> listOfNames;
+        
         public MainWindow()
         {
             InitializeComponent();
+
+            // Initialize the list with the placeholder names
+            List<string> list =
+            [
+                "Ziggy Stardust",
+                "Frodo Baggins",
+                "Darth Vader",
+                "Galadriel Silverleaf",
+                "Sparky McFluff",
+                "Waldo B. Lost",
+                "กาญจนา McSix",
+                "Gandalf the Grey",
+                "Ulysses 'Snakehands' McDougall",
+                "Venkatanarasimharajuvaripeta Wumpus",
+                "Shivankumaraswamy Krishnamurthy Raghunath",
+                "الحسيني",
+                "Muhammad Abdelrahman ibn Al-Mahmoud al-Farouq",
+                "Papadopoulos-Alexandropoulos Firestorm",
+                "明张",
+                "Pipkin Puddleduck",
+                "Aleksandrovich Dimitrov Petrovskaya Ivanov",
+                "Per-Göran Karlsson von Heidenstam af Skånesläkten",
+                "Wiggles Snickerbottom",
+                "Zephyr Nightwind",
+                "Doodlebug Sparklestep",
+                "Sir Adrian Carton de Wiart",
+                "Tinkerbell Twinkletoes",
+                "Bo Li",
+                "Dinglehopper Wobblesworth",
+                "Kǎi McQuirk",
+                "Fizzlewhit Wobblebottom",
+                "鈴木 健太",
+                "Jo Wu",
+                "Le To",
+                "Örjan Johansson Florist",
+                "Främling Skådespelare",
+                "Émil Låås",
+            ];
+            listOfNames = list;
+
+            foreach (string name in listOfNames)
+            {
+                ListBoxClassList.Items.Add(name);
+            }
         }
     }
 }
