4774a6331fdece4ce058041dc8db3d97ae94ed2b
JCTe4 <jesper.cejie@elev.ga.ntig.se>
2024-10-22 13:59:45 +0200

    added file for testing that seating is correct

---
+++ Classroom-Seating-Planner/Tests/TestSeating.cs
@@ -0,0 +1,28 @@
+﻿using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Conditions;
+using FlaUI.UIA3;
+using System.Diagnostics;
+using FlaUIElement = FlaUI.Core.AutomationElements;
+namespace Tests
+{
+    [TestClass]
+    public class TestSeating
+    {
+        [TestMethod]
+        public void TestSeatingIsCorrect()
+        {
+            // Find and run the application
+            FlaUI.Core.Application app = FlaUI.Core.Application.Launch("..\\..\\..\\..\\Classroom-Seating-Planner\\bin\\Debug\\net8.0-windows\\Classroom-Seating-Planner.exe");
+            using FlaUI.UIA3.UIA3Automation automation = new();
+
+            // Find the main window for the purpose of finding elements
+            FlaUIElement.Window window = app.GetMainWindow(automation);
+            ConditionFactory cf = new(new UIA3PropertyLibrary());
+
+            // Write your test after this comment!
+
+            // Write your test before this comment!
+            app.Close();
+        }
+    }
+}
