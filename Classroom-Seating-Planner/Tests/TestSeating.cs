b83013bed5832ed4589d999ad0b230f545d61e84
JCTe4 <jesper.cejie@elev.ga.ntig.se>
2024-10-24 15:59:50 +0200

    placed assets of testseating in loops based on predefined test indexes

--- Classroom-Seating-Planner/Tests/TestSeating.cs
+++ Classroom-Seating-Planner/Tests/TestSeating.cs
@@ -21,25 +21,38 @@ namespace Tests
             FlaUIElement.Window window = app.GetMainWindow(automation);
             ConditionFactory cf = new(new UIA3PropertyLibrary());
 
+            int[] testIndex = { 1, 16, 32 }; // Get indexes by length of student list? assuming the empty seates will always be the last ones
+
             // Find all the seats
             List<FlaUIElement.AutomationElement> allSeats = Utils.GetAllByAutomationId(window, cf, "Seat", FlaUI.Core.Definitions.ControlType.Text);
 
             // Read that some of them are empty
             string errorMessage = "Some of the seats are not empty before hitting the randomize button";
-            Assert.IsTrue(allSeats[0].Name.Equals(string.Empty), errorMessage);
-            Assert.IsTrue(allSeats[16].Name.Equals(string.Empty), errorMessage);
-            Assert.IsTrue(allSeats[32].Name.Equals(string.Empty), errorMessage);
+            foreach (int index in testIndex)
+            {
+                Assert.IsTrue(allSeats[index].Name.Equals(string.Empty), errorMessage);
+            }
+            //Assert.IsTrue(allSeats[0].Name.Equals(string.Empty), errorMessage);
+            //Assert.IsTrue(allSeats[16].Name.Equals(string.Empty), errorMessage);
+            //Assert.IsTrue(allSeats[32].Name.Equals(string.Empty), errorMessage);
 
             Utils.ClickRandomizeSeatingButton(window, cf);
 
             // Get the seats again
             allSeats = Utils.GetAllByAutomationId(window, cf, "Seat", FlaUI.Core.Definitions.ControlType.Text);
 
+            // Get array of names to compare agains list of seats
+            string[] allNames = ListActions.GetListBoxItemsAsArray(window, cf, "StudentList");
+
             // Read that some of the seats are not empty
             errorMessage = "Some of the seats are empty after hitting the randomize button";
-            Assert.IsFalse(allSeats[0].Name.Equals(string.Empty), errorMessage);
-            Assert.IsFalse(allSeats[16].Name.Equals(string.Empty), errorMessage);
-            Assert.IsFalse(allSeats[32].Name.Equals(string.Empty), errorMessage); // Get index by length of student list? assuming the empty seates will always be the last ones
+            foreach (int index in testIndex)
+            {
+                Assert.IsTrue(allSeats[index].Name.Equals(allNames[index]), errorMessage);
+            }
+            //Assert.IsTrue(allSeats[0].Name.Equals(allNames[0]), errorMessage);
+            //Assert.IsTrue(allSeats[16].Name.Equals(allNames[16]), errorMessage);
+            //Assert.IsTrue(allSeats[32].Name.Equals(allNames[32]), errorMessage); 
 
             app.Close();
         }
