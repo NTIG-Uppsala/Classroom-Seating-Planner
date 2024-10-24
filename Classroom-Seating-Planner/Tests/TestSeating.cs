0066d347bee640419873eb62e25506596d1b178b
JCTe4 <jesper.cejie@elev.ga.ntig.se>
2024-10-24 09:34:52 +0200

    wrote pseudo-code for testing that assigning seating works

--- Classroom-Seating-Planner/Tests/TestSeating.cs
+++ Classroom-Seating-Planner/Tests/TestSeating.cs
@@ -22,7 +22,11 @@ namespace Tests
             ConditionFactory cf = new(new UIA3PropertyLibrary());
 
             // Write your test after this comment!
-
+            
+            List<FlaUIElement.Label> seatLabels = window.FindAllDescendants(cf.By)
+            // Check that order of student name list is the same as the order of the seating
+                // Get order of student name list through method in listactions
+                // Get the order of the labels with the tag "Seat"
             // Write your test before this comment!
             app.Close();
         }
