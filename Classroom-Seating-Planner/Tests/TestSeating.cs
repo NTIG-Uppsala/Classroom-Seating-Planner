98b420e8476da927994a0133abe171da0c5b80a0
viggoStrom <viggostrom05@gmail.com>
2024-10-24 14:12:39 +0200

    is able to find elements by their names

--- Classroom-Seating-Planner/Tests/TestSeating.cs
+++ Classroom-Seating-Planner/Tests/TestSeating.cs
@@ -1,9 +1,7 @@
-﻿using FlaUI.Core.AutomationElements;
-using FlaUI.Core.Conditions;
+﻿using FlaUI.Core.Conditions;
 using FlaUI.UIA3;
 using System.Diagnostics;
 using FlaUIElement = FlaUI.Core.AutomationElements;
-using ListActions = Classroom_Seating_Planner.ListActions;
 
 namespace Tests
 {
@@ -14,20 +12,32 @@ namespace Tests
         public void TestSeatingIsCorrect()
         {
             // Find and run the application
-            FlaUI.Core.Application app = FlaUI.Core.Application.Launch("..\\..\\..\\..\\Classroom-Seating-Planner\\bin\\Debug\\net8.0-windows\\Classroom-Seating-Planner.exe");
+            FlaUI.Core.Application app = FlaUI.Core.Application.Launch("..\\..\\..\\..\\Classroom-Seating-Planner\\bin\\Debug\\net8.0-windows\\win-x64\\Classroom-Seating-Planner.exe");
             using FlaUI.UIA3.UIA3Automation automation = new();
 
             // Find the main window for the purpose of finding elements
             FlaUIElement.Window window = app.GetMainWindow(automation);
             ConditionFactory cf = new(new UIA3PropertyLibrary());
 
-            // Write your test after this comment!
-            
-            //List<FlaUIElement.Label> seatLabels = window.FindAllDescendants(cf.By)
-            // Check that order of student name list is the same as the order of the seating
-                // Get order of student name list through method in listactions
-                // Get the order of the labels with the tag "Seat"
-            // Write your test before this comment!
+            var allElements = window.FindAllDescendants(cf.ByFrameworkId("WPF")).ToList();
+
+            var allSeats = allElements.Where(element =>
+            {
+                return (
+                    !string.IsNullOrEmpty(element.AutomationId)
+                    &&
+                    element.AutomationId.Contains("Seat")
+                    &&
+                    element.ControlType.Equals(FlaUI.Core.Definitions.ControlType.Text)
+                );
+            }).ToList();
+
+            foreach (var seat in allSeats)
+            {
+                Trace.WriteLine(seat);
+            }
+            Trace.WriteLine(allSeats.Count);
+
             app.Close();
         }
     }
