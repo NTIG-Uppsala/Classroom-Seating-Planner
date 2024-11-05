48e15f0729619f648dc141aeda5b8c00cb35fb1b
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2024-11-05 11:57:01 +0100

    Added generic getDataFromFile method that GetStudentNamesFromFile uses to fetch student list

--- Classroom-Seating-Planner/Classroom-Seating-Planner/src/FileHandler.cs
+++ Classroom-Seating-Planner/Classroom-Seating-Planner/src/FileHandler.cs
@@ -4,7 +4,22 @@ namespace Classroom_Seating_Planner.src
 {
     public class FileHandler
     {
-        // Public method for fetching student names from an external file
+        // Public method for fetching data from an external file and returning it as a list
+        public static List<string> GetDataFromFile(string filePath)
+        {
+            // Read the data from the file and return it as a list
+            using StreamReader reader = new(filePath);
+            List<string> data = reader
+                .ReadToEnd()
+                .Split("\n")
+                .Select(item => item.Trim())
+                .Where(item => !string.IsNullOrEmpty(item))
+                .ToList();
+            return data;
+        }
+
+
+        // Public method for fetching student names from an external file and returning them as a list
         public static List<string> GetStudentNamesFromFile()
         {
             // Get the paths to the app's directory and the names file
@@ -12,14 +27,7 @@ namespace Classroom_Seating_Planner.src
             string filePath = Path.Combine(documentsFolder, "Bordsplaceringsgeneratorn", "klasslista.txt");
 
             // Read the names from the file and return them as a list
-            using StreamReader reader = new(filePath);
-            List<string> names = reader
-                .ReadToEnd()
-                .Split("\n")
-                .Select(name => name.Trim())
-                .Where(name => !string.IsNullOrEmpty(name))
-                .ToList();
-            return names;
+            return GetDataFromFile(filePath);
         }
     }
 }
