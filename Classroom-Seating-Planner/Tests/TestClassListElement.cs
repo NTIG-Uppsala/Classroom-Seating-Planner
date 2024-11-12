a2d84fba46b207827b4351e14b2d8951cbddaa01
JCTe4 <jesper.cejie@elev.ga.ntig.se>
2024-11-12 10:35:22 +0100

    Refactored 'RandomizeTestingClassListTest'-method

--- Classroom-Seating-Planner/Tests/TestClassListElement.cs
+++ Classroom-Seating-Planner/Tests/TestClassListElement.cs
@@ -8,6 +8,11 @@ namespace Tests
         [TestMethod]
         public void RandomizeTestingClassListTest()
         {
+            // Set up/start the test
+            (FlaUI.Core.Application app, FlaUI.UIA3.UIA3Automation automation, FlaUIElement.Window window, FlaUI.Core.Conditions.ConditionFactory cf)
+                = Utils.SetUp();
+
+
             bool hasClassListOrderChanged(List<string> classListOld, List<string> classListNew)
             {
                 return !classListOld.SequenceEqual(classListNew);
@@ -27,44 +32,27 @@ namespace Tests
                 return false;
             }
 
-            List<string> getClassListFromClassListElementBeforeShuffle(FlaUIElement.Window window, FlaUI.Core.Conditions.ConditionFactory cf)
-            {
-                // Extract an list of students from the ListBox element in the UI
-                return Utils.XAMLManager.GetClassListFromElement(window, cf);
-            }
+            // Get class list before shuffle
+            List<string> classListOld = Utils.XAMLManager.GetClassListFromElement(window, cf);
 
-            List<string> clickRandomizeButtonAndGetNewList(FlaUIElement.Window window, FlaUI.Core.Conditions.ConditionFactory cf)
-            {
-                Utils.XAMLManager.ClickRandomizeSeatingButton(window, cf);
-
-                // Extract an list of students from the ListBox element in the UI
-                List<string> classListNew = Utils.XAMLManager.GetClassListFromElement(window, cf);
-
-                return classListNew;
-            }
-
-            // Set up/start the test
-            (FlaUI.Core.Application app, FlaUI.UIA3.UIA3Automation automation, FlaUIElement.Window window, FlaUI.Core.Conditions.ConditionFactory cf)
-                = Utils.SetUp();
-
-
-            List<string> classListOld = getClassListFromClassListElementBeforeShuffle(window, cf);
-            List<string> classListNew = clickRandomizeButtonAndGetNewList(window, cf);
-
-            // Custom error messages for asserts
-            string errorMessageClassListOrderUnchanged = "Test failed because the order of the class list has not changed.";
-            string errorMessageClassListIsDifferent = "Test failed because the content of the generated class list is not the same as the list before generating";
+            // Click randomize-button and get class list after shuffle
+            Utils.XAMLManager.ClickRandomizeSeatingButton(window, cf);
+            List<string> classListNew = Utils.XAMLManager.GetClassListFromElement(window, cf);
 
             // Trigger the randomizing function and assert that a new, randomized, class list is generated and make sure all the names are the same
-            Assert.IsTrue(hasClassListOrderChanged(classListOld, classListNew), errorMessageClassListOrderUnchanged);
-            Assert.IsFalse(hasClassListContentChanged(classListOld, classListNew), errorMessageClassListIsDifferent);
+            Assert.IsTrue(hasClassListOrderChanged(classListOld, classListNew), "Test failed because the order of the class list has not changed.");
+            Assert.IsFalse(hasClassListContentChanged(classListOld, classListNew), "Test failed because the content of the generated class list is not the same as the list before generating");
 
-            classListOld = getClassListFromClassListElementBeforeShuffle(window, cf);
-            classListNew = clickRandomizeButtonAndGetNewList(window, cf);
+            // Get class list before shuffle
+            classListOld = Utils.XAMLManager.GetClassListFromElement(window, cf);
+
+            // Click randomize-button and get class list after shuffle
+            Utils.XAMLManager.ClickRandomizeSeatingButton(window, cf);
+            classListNew = Utils.XAMLManager.GetClassListFromElement(window, cf);
 
             // Test one more time to make sure that the list can be scrambled again
-            Assert.IsTrue(hasClassListOrderChanged(classListOld, classListNew), errorMessageClassListOrderUnchanged);
-            Assert.IsFalse(hasClassListContentChanged(classListOld, classListNew), errorMessageClassListIsDifferent);
+            Assert.IsTrue(hasClassListOrderChanged(classListOld, classListNew), "Test failed because the order of the class list has not changed.");
+            Assert.IsFalse(hasClassListContentChanged(classListOld, classListNew), "Test failed because the content of the generated class list is not the same as the list before generating");
 
 
             Utils.TearDown(app);
