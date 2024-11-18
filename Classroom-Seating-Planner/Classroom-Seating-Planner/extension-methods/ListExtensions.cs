13f6655b0ee056af4c4304b5abf6aa3b1ab634e9
JCTe4 <jesper.cejie@elev.ga.ntig.se>
2024-11-18 11:01:58 +0100

    Added comments to "Shuffle"-method
    Co-authored-by: Klotarn <klotarn.swe@gmail.com>
    Co-authored-by: viggoStrom <viggostrom05@gmail.com>
    Co-authored-by: Axel Thornberg <axel.thornberg@elev.ga.ntig.se>

--- Classroom-Seating-Planner/Classroom-Seating-Planner/extension-methods/ListExtensions.cs
+++ Classroom-Seating-Planner/Classroom-Seating-Planner/extension-methods/ListExtensions.cs
@@ -2,14 +2,17 @@
 
 namespace ExtensionMethods
 {
+    // Methods that extend the List class
     public static class ListExtensions
     {
         public static List<T> Shuffle<T>(this List<T> list)
         {
+            // If the list is empty or has only one element, return the list as is
             if (list.Count < 2) return list;
 
             Random rng = new();
 
+            // Store the list before shuffling for comparison
             List<T> oldList = list;
 
             // Fisher-Yates shuffle algorithm to shuffle the list in place
@@ -19,6 +22,7 @@ namespace ExtensionMethods
                 (list[i], list[j]) = (list[j], list[i]);
             }
 
+            // If the list is the same as before shuffling, swap the first two elements
             if (oldList.SequenceEqual(list))
             {
                 (list[0], list[1]) = (list[1], list[0]);
