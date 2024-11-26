fce55b0779439d10b3dcae897d5e75749b71a905
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2024-11-26 14:10:33 +0100

    Added distance method to get diagonal distance between two cells

---
+++ Classroom-Seating-Planner/Classroom-Seating-Planner/Src/ConstraintsHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classroom_Seating_Planner.Src
+{
+    internal class ConstraintsHandler
+    {
+        public static double GetDistanceBetweenCells(Classroom_Seating_Planner.Cells.Cell cell1, Classroom_Seating_Planner.Cells.Cell cell2)
+        {
+            // Get the horizontal and vertical distance between the two cells
+            double horizontalDistance = Math.Abs(cell1.centerX - cell2.centerX);
+            double verticalDistance = Math.Abs(cell1.centerY - cell2.centerY);
+
+            // Use the Pythagorean theorem to calculate the distance between the two cells
+            return Math.Sqrt(Math.Pow(horizontalDistance, 2) + Math.Pow(verticalDistance, 2)); ;
+        }
+    }
+}
