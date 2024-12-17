2de4af70937218948360933fc129841d407f5ecc
Klotarn <klotarn.swe@gmail.com>
2024-12-17 10:46:24 +0100

    Moved interpretedConstraints() into project
    Co-authored-by: viggoStrom <viggostrom05@gmail.com>

--- Classroom-Seating-Planner/Classroom-Seating-Planner/Src/FileHandler.cs
+++ Classroom-Seating-Planner/Classroom-Seating-Planner/Src/FileHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
@@ -38,7 +39,7 @@ namespace Classroom_Seating_Planner.Src
         public struct Constraint
         {
             public string type;
-            public List<string?> argmuent;
+            public List<string?> arguments;
             public int priority;
         }
 
@@ -51,7 +52,71 @@ namespace Classroom_Seating_Planner.Src
 
         public static List<Constraint>? InterpretStudentConstraints(string studentName, string rawConstraints)
         {
-            return null;
+            Dictionary<string, Constraint> functionLookupTable = new() {
+                { "nära",         new Constraint { type = "distance", arguments = [studentName, "near", null], priority = 1 }},
+                { "intenära",     new Constraint { type = "distance", arguments = [studentName, "far",  null], priority = 1 }},
+                { "inte nära",    new Constraint { type = "distance", arguments = [studentName, "far",  null], priority = 1 }},
+                { "långtfrån",    new Constraint { type = "distance", arguments = [studentName, "far",  null], priority = 1 }},
+                { "långt från",   new Constraint { type = "distance", arguments = [studentName, "far",  null], priority = 1 }},
+                { "bredvid",      new Constraint { type = "adjacent", arguments = [studentName, "yes",  null], priority = 1 }},
+                { "intebredvid",  new Constraint { type = "adjacent", arguments = [studentName, "no",   null], priority = 1 }},
+                { "inte bredvid", new Constraint { type = "adjacent", arguments = [studentName, "no",   null], priority = 1 }},
+            };
+
+            Dictionary<string, string> recipientLookupTable = new() {
+                {"tavlan",       "whiteboardCover"},
+                {"tavla",        "whiteboardCover"},
+                {"whiteboard",   "whiteboardCover"},
+                {"whiteboards",  "whiteboardCover"},
+                {"svartatavlan", "whiteboardCover"},
+                {"klösbrädan",   "whiteboardCover"},
+                {"dörren",       "door"},
+                {"dörr",         "door"},
+                {"fönstret",     "window"},
+                {"fönster",      "window"},
+                {"vindöga",      "window"},
+            };
+
+            List<Constraint> interpretedConstraints = [];
+
+            List<string> rawConstraintsList = rawConstraints.Split('/').Select(rawConstraint => rawConstraint.Trim()).ToList();
+
+            rawConstraintsList.ForEach(rawConstraint =>
+            { // TODO - Whitespace remover Regex.Replace(input, @"\s", "")
+                string trimmedConstraint = rawConstraint.ToLower();
+                string? functionName = functionLookupTable.Keys
+                    .ToList()
+                    .Where(functionName => trimmedConstraint.StartsWith(functionName))
+                    .FirstOrDefault();
+
+                if (functionName == null) return;
+
+                Constraint interpretedConstraint = functionLookupTable[functionName];
+
+                // Isolate the recipient string
+                string recipient = Regex.Replace(rawConstraint, @"\(.*\) ", "") // Remove priority (N)
+                    .Replace(functionName, "") // TODO - whitespace remover, this is linked to the whitespace remover mentioned above
+                    .Trim();
+
+                // Look if the recipent is in the recipientLookupTable
+                if (recipientLookupTable[Regex.Replace(recipient, @"\s", "").ToLower()] != null)
+                {
+                    recipient = recipientLookupTable[recipient];
+                }
+                interpretedConstraint.arguments[2] = recipient;
+
+                // Find the priority of the constraint
+                // Priority is the number insite the parenthesis (N)
+                System.Text.RegularExpressions.Match match = Regex.Match(rawConstraint, @"\(([^)]+)\)");
+                if (match.Success && int.TryParse(match.Groups[1].Value, out int intValue))
+                {
+                    interpretedConstraint.priority = intValue; // Override default priority value
+                }
+
+                interpretedConstraints.Add(interpretedConstraint);
+            });
+
+            return interpretedConstraints;
         }
 
         // Returns the list of student names read from an external file as a list
